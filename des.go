388131b470c5ee789c28ad8bed911290d66d513e
xiaj <xiajia08@gmail.com>
2020-02-11 14:59:29 +0800

    封装加密解密函数

--- des.go
+++ des.go
@@ -60,3 +60,27 @@ func (lbs *DesLbs) StrDec(data, key1, key2, key3 string) (result string) {
 	result = value.String()
 	return
 }
+
+func (lbs *DesLbs) NaturalOrdering(data []string) (result string) {
+	filePath := "./js/des.js"
+	//先读入文件内容
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	vm := otto.New()
+
+	_, err = vm.Run(string(bytes))
+	if err != nil {
+		panic(err)
+	}
+
+	value, err := vm.Call("naturalOrdering", nil, data)
+	if err != nil {
+		panic(err)
+	}
+
+	result = value.String()
+	return
+}
